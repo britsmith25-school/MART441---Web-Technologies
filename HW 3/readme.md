470295251ab0939e89ddf5d1b23f688c8c488f1e
Brittany Smith <157831161+britsmith25-school@users.noreply.github.com>
2026-02-01 15:00:35 -0700

    HW3 Not the AI Version

---
+++ HW 3/readme.md
@@ -0,0 +1,11 @@
+For this assignment, I created 2 seperate folders, "HW 3" is my design, and "HW 3 - Gemini" was the generated files I got from the Gemini AI, where I copied and pasted the prompt from the homework assignment, and added that "complete this prompt in any way you see fit. you have an index.html file, a script.js file, and an "images" folder should you need it. 
+
+If you open mine, it's a little bit more colorful, and also a little bit more basic because I was more focused on the JavaScript portion than the HTML portion. The AI's version was clean for the most part, but also a little bit more clinical feeling, as it used greys and whites with it. 
+
+When it came to their answers for the console.log prompt, they had some interesting answers. Mine, while maybe not as interesting, actually felt like me. 
+
+If you relied only on AI-generated outputs, or someone elses' work, you wouldn't be able to get the sense of pride from learning from it. It's just there. Yeah, mine may seem simple, but hey I like my aquamarine background and my Ray Volpe plaing on my headset. 
+
+Because I did the prompt after I did my own work, I would mostly explain the code that Gemini wrote, but if it was reversed I probably coun't tell you a whole lot outside of the comments it put in there.
+
+My final work feels like me, the Gemini's work does not at all. Like for example, chess is not a game I enjoy very much. And Elden Ring? Are they okay?
---
+++ HW 3/script.js
@@ -0,0 +1,21 @@
+//Favorite Websites
+console.log("Top Favorite Websites");
+console.log("1: Reddit.com - I scroll this daily just to find the funny ramblings that people have to say about anything and everything.");
+console.log("2: Wowhead.com - I'm on this one more than I would like to admit, but it's a great resource for World of Warcraft information.");
+console.log("3: Facebook Marketplace - for finding stuff that I can't live without.");
+
+//Favorite Games
+console.log("Top Favorite Games");
+console.log("1: World of Warcraft - Been playing this game on and off since about 2008, has been a significant part of my gaming life.");
+console.log("2: Baulder's Gate 3 - My boyfriend recently got me into this since it's DnD based, and has a pretty solid story");
+console.log("3: Phasmophobia - A fun co-op horror game that I haven't played as much recently, but the jump scares are intense and fun.");
+
+//Favorite Artists
+console.log("Top Favorite Artists");
+console.log("1: Hans Zimmer - the Interstellar soundtrack is one of my all time favorites.");
+console.log("2: Breaking Benjamin - I grew up listening to these guys, and I'm still mad I was unable to see them when they came to town last with Daughtery.");
+console.log("3: Ray Volpe - My go-to EDM atist, his music gets turned on when the stress gets too high and I need to chill out.");
+
+//Question - Answer Prompt
+let UserAnswer = prompt("What is one of your favorite Artists?");
+console.log("One of my favorite artists is " + UserAnswer + "!");
